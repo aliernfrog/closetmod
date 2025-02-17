152c796d05524ebb8e7f08e54ce53fcfe691c6a4
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2025-02-17 21:33:35 +0300

    feat(crosspost): use attachment urls if forwarding fails

--- src/channels/pf-crosspost.js
+++ src/channels/pf-crosspost.js
@@ -1,10 +1,25 @@
-import { AttachmentBuilder, MessageFlags } from "discord.js";
+import { MessageFlags } from "discord.js";
+
+const FORWARD_TO = "855250425794920448"; // closet news channel
 
 export default {
   id: "1003647447135965216",
-  async execute(client, message) {
+  execute(client, message) {
     if (!message.flags.has(MessageFlags.IsCrosspost)) return;
-    const forwarded = await message.forward("855250425794920448");
-    await forwarded.crosspost();
+    
+    message.forward(FORWARD_TO)
+      .then(msg => msg.crosspost())
+      .catch(async () => {
+        const channel = await client.channels.fetch(FORWARD_TO);
+        channel.send({
+          content: [
+            message.content?.length ? `${message.content}\n` : "",
+            message.attachments?.map?.(a => a.url)?.join?.("\n") ?? "",
+            `\n-# [Forwarded from Polyfield server](https://discord.com/channels/${message.channel.id}/${message.id})`
+          ].join(""),
+          embeds: message.embeds,
+          allowedMentions: { parse: [] }
+        }).then(msg => msg.crosspost());
+      });
   }
 }
