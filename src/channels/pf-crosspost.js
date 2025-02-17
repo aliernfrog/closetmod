90660d18f401e1a9780217602adfa77266c3e870
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2025-02-17 18:55:25 +0300

    feat(crosspost): use new forward feature

--- src/channels/pf-crosspost.js
+++ src/channels/pf-crosspost.js
@@ -5,7 +5,8 @@ export default {
   execute(client, message) {
     if (!message.flags.has(MessageFlags.IsCrosspost)) return;
     const channel = client.channels.cache.get("855250425794920448");
-    message.content = `${message.content ?? ""}\n\n-# Forwarded from Polyfield server`;
+    message.forward(channel);
+    /*message.content = `${message.content ?? ""}\n\n-# Forwarded from Polyfield server`;
     
     channel.send({
       allowedMentions: { parse: [] },
@@ -18,6 +19,6 @@ export default {
         content: message.content+message.attachments?.map(a => `\n${a.url}`),
         embeds: message.embeds
       }).then(msg => msg.crosspost());
-    });
+    });*/
   }
 }
