f72a19d9c3cae4e11327af44dcd3690bc4e93ebe
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2025-02-24 22:46:40 +0300

    fix(crosspost): workaround posting twice

--- src/channels/pf-crosspost.js
+++ src/channels/pf-crosspost.js
@@ -7,9 +7,14 @@ export default {
   execute(client, message) {
     if (!message.flags.has(MessageFlags.IsCrosspost)) return;
     
+    let forwarded;
     message.forward(FORWARD_TO)
-      .then(msg => msg.crosspost())
+      .then(msg => {
+        forwarded = true;
+        msg.crosspost(); // TODO findout why this fails
+      })
       .catch(async () => {
+        if (forwarded) return;
         const channel = await client.channels.fetch(FORWARD_TO);
         channel.send({
           content: [
