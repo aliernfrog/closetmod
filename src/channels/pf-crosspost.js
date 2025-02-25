0221e02ef466b8478ad8b76b0327522f6c75bfa0
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2025-02-25 21:57:31 +0300

    fix(crosspost): ditch forwarding

--- src/channels/pf-crosspost.js
+++ src/channels/pf-crosspost.js
@@ -4,27 +4,18 @@ const FORWARD_TO = "855250425794920448"; // closet news channel
 
 export default {
   id: "1003647447135965216",
-  execute(client, message) {
+  async execute(client, message) {
     if (!message.flags.has(MessageFlags.IsCrosspost)) return;
     
-    let forwarded;
-    message.forward(FORWARD_TO)
-      .then(msg => {
-        forwarded = true;
-        msg.crosspost(); // TODO findout why this fails
-      })
-      .catch(async () => {
-        if (forwarded) return;
-        const channel = await client.channels.fetch(FORWARD_TO);
-        channel.send({
-          content: [
-            message.content?.length ? `${message.content}\n` : "",
-            message.attachments?.size ? message.attachments?.map?.(a => a.url)?.join?.("\n")+"\n" : "",
-            "\n-# Forwarded from Polyfield server"
-          ].join(""),
-          embeds: message.embeds,
-          allowedMentions: { parse: [] }
-        }).then(msg => msg.crosspost());
-      });
+    const channel = await client.channels.fetch(FORWARD_TO);
+    channel.send({
+      content: [
+        message.content?.length ? `${message.content}\n` : "",
+        message.attachments?.size ? message.attachments?.map?.(a => a.url)?.join?.("\n")+"\n" : "",
+        "\n-# Forwarded from Polyfield server"
+      ].join(""),
+      embeds: message.embeds,
+      allowedMentions: { parse: [] }
+    }).then(msg => msg.crosspost());
   }
 }
