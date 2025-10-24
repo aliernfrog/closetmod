bc6d9d2ffca1d99678f391dbd6d630a2dbe5158d
alieRN <45766489+aliernfrog@users.noreply.github.com>
2025-10-24 19:30:27 +0300

    feat(mousetrap): dm user if available

--- src/channels/mc-mousetrap.js
+++ src/channels/mc-mousetrap.js
@@ -9,6 +9,16 @@ export default {
       await message.delete();
     } catch {}
     
+    try {
+      await message.author.send([
+        `### You were temporarily kicked from **${message.guild.name}**`,
+        "You sent a message in a channel in which we clearly stated that sending a message will perform an instant ban.",
+        "**If you do not remember doing this**, your account may have been compromised. Please secure your account immediately.",
+        "",
+        "-# You're welcome to rejoin: https://discord.gg/aQhGqHSc3W"
+      ].join("\n"));
+    } catch {}
+    
     await message.member.ban({
       deleteMessageSeconds: 600,
       reason: reason
